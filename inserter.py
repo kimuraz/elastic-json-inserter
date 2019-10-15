0da6855bccc17fe9cc4062c74271e13c4e433af7
Kimura <j.eduardokimura@icloud.com>
2019-10-15 23:48:59 +0200

    Add some logs

--- inserter.py
+++ inserter.py
@@ -2,20 +2,20 @@ import yaml
 import json
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch()
-
 def gen_data(arr, index):
-    for reg in arr:
+    for idx, reg in enumerate(arr):
+        print('Inserting register #{} on index: {}'.format(idx+1, index))
         yield {
-            "_index": index,
-            "_type": "document",
-            "doc": reg,
+            '_index': index,
+            '_type': 'document',
+            'doc': reg,
         }
 
 with open('settings.yml', 'r') as settings_file:
     try:
         settings = yaml.safe_load(settings_file)
         with open(settings['file'], 'r') as json_arr:
+            es = Elasticsearch()
             helpers.bulk(es, gen_data(json.load(json_arr), settings['elastic_index']))
     except yaml.YAMLError as e:
         print('Yaml {}'.format(e))
